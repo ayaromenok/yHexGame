add5eef17f938676e602ee0dfb147e2a55c31b39
Andrey Yaromenok <ayaromenok@gmail.com>
2020-08-23 09:28:47 +0300

    #1 basic layout with stubs to qwidget app

--- src/mainwidget.cpp
+++ src/mainwidget.cpp
@@ -1,11 +1,33 @@
 #include "mainwidget.h"
+#include <QtWidgets>
 
 MainWidget::MainWidget(QWidget *parent)
-    : QWidget(parent)
-{
+    : QWidget(parent) {
+    initLayout();
 }
 
-MainWidget::~MainWidget()
-{
+MainWidget::~MainWidget() {
 }
 
+void
+MainWidget::initLayout() {
+    setGeometry(0,0,640,320);
+    _loMain = new QVBoxLayout(this);
+    _loGame = new QHBoxLayout(this);
+
+    _lbMenu = new QLabel("Menu", this);
+    _lbStatusBar = new QLabel("Status Bar", this);
+    _lbGame2D = new QLabel("Game 2D", this);
+    _lbGame3D = new QLabel("Game 3D", this);
+    _lbConsole = new QLabel("Console", this);
+
+    _loGame->addWidget(_lbGame2D);
+    _loGame->addWidget(_lbGame3D);
+    _loGame->addWidget(_lbConsole);
+
+    _loMain->addWidget(_lbMenu);
+    _loMain->addLayout(_loGame);
+    _loMain->addWidget(_lbStatusBar);
+
+    this->setLayout(_loMain);
+}
--- src/mainwidget.h
+++ src/mainwidget.h
@@ -2,6 +2,9 @@
 #define MAINWIDGET_H
 
 #include <QWidget>
+class QVBoxLayout;
+class QHBoxLayout;
+class QLabel;
 
 class MainWidget : public QWidget
 {
@@ -10,5 +13,15 @@ class MainWidget : public QWidget
 public:
     MainWidget(QWidget *parent = nullptr);
     ~MainWidget();
+
+private:
+    void initLayout();
+    QVBoxLayout*    _loMain;
+    QHBoxLayout*    _loGame;
+    QLabel*         _lbMenu;
+    QLabel*         _lbStatusBar;
+    QLabel*         _lbGame2D;
+    QLabel*         _lbGame3D;
+    QLabel*         _lbConsole;
 };
 #endif // MAINWIDGET_H
