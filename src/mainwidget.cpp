e03f351b69b1390752a271fd7d1b18bc2923c2a4
Andrey Yaromenok <ayaromenok@gmail.com>
2020-08-23 09:52:50 +0300

    #1 add object names and fix hierarchy

--- src/mainwidget.cpp
+++ src/mainwidget.cpp
@@ -12,14 +12,22 @@ MainWidget::~MainWidget() {
 void
 MainWidget::initLayout() {
     setGeometry(0,0,640,320);
-    _loMain = new QVBoxLayout(this);
-    _loGame = new QHBoxLayout(this);
 
-    _lbMenu = new QLabel("Menu", this);
-    _lbStatusBar = new QLabel("Status Bar", this);
-    _lbGame2D = new QLabel("Game 2D", this);
-    _lbGame3D = new QLabel("Game 3D", this);
-    _lbConsole = new QLabel("Console", this);
+    _loMain = new QVBoxLayout(this);
+    _loMain->setObjectName("layout/Main");
+    _loGame = new QHBoxLayout();
+    _loGame->setObjectName("layout/Game");
+
+    _lbMenu = new QLabel("Menu");
+    _lbMenu->setObjectName("label/Menu");
+    _lbStatusBar = new QLabel("Status Bar");
+    _lbStatusBar->setObjectName("label/StatusBar");
+    _lbGame2D = new QLabel("Game 2D");
+    _lbGame2D->setObjectName("label/Game 2D");
+    _lbGame3D = new QLabel("Game 3D");
+    _lbGame3D->setObjectName("label/Game 3D");
+    _lbConsole = new QLabel("Console");
+    _lbConsole->setObjectName("label/Console");
 
     _loGame->addWidget(_lbGame2D);
     _loGame->addWidget(_lbGame3D);
@@ -29,5 +37,5 @@ MainWidget::initLayout() {
     _loMain->addLayout(_loGame);
     _loMain->addWidget(_lbStatusBar);
 
-    this->setLayout(_loMain);
+    //dumpObjectTree();
 }
